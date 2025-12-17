035d91b5d109a849f4735f1d8f22b89945850235
Ayush Singh <ayushsingh123102@gmail.com>
2025-12-17 23:19:00 +0530

    updated admin panel

--- Backend/controllers/resourcecontrollers.js
+++ Backend/controllers/resourcecontrollers.js
@@ -93,10 +93,25 @@ const deleteResource = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+const getAdminStats = async (req, res) => {
+  try {
+    const totalResources = await Resource.countDocuments();
+    const availableResources = await Resource.countDocuments({
+      availability: { $gt: 0 }
+    });
 
+    res.json({
+      totalResources,
+      availableResources
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to fetch stats" });
+  }
+};
 module.exports = {
   uploadResource,
   getAllResources,
   updateResource,
-  deleteResource
+  deleteResource,
+  getAdminStats
 };
--- Backend/routes/resourceroutes.js
+++ Backend/routes/resourceroutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../config/multer");
-const {uploadResource,getAllResources,updateResource,deleteResource} = require("../controllers/resourcecontrollers");
+const {uploadResource,getAllResources,updateResource,deleteResource,getAdminStats} = require("../controllers/resourcecontrollers");
 router.post("/upload",upload.single("pdf"),uploadResource);
 router.get("/view",getAllResources);
+router.get("/stats",getAdminStats);
 router.put("/update/:id",upload.single("pdf"),updateResource);
 router.delete("/:id",deleteResource);
 module.exports = router;

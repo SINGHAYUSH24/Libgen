744403e4cfd317cdf3d798335a816f67ae76c6e9
Ayush Singh <ayushsingh123102@gmail.com>
2025-12-23 15:35:08 +0530

    commited

--- Backend/controllers/resourcecontrollers.js
+++ Backend/controllers/resourcecontrollers.js
@@ -30,7 +30,7 @@ const uploadResource = async (req, res) => {
 };
 const getAllResources = async (req, res) => {
   try {
-    const resources = await Resource.find().sort({ createdAt: -1 }).limit(5);
+    const resources = await Resource.find().sort({ createdAt: -1 });
     res.json(resources);
   } catch (err) {
     res.status(500).json({ error: err.message });

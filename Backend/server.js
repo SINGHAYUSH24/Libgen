ca70ffac3fc25617ddd55797aa0c2e57dcb4f6bd
Ayush Singh <ayushsingh123102@gmail.com>
2025-12-24 02:02:49 +0530

    limited cors access

--- Backend/server.js
+++ Backend/server.js
@@ -14,10 +14,15 @@ dotenv.config();
 
 const app = express();
 db_connect();
-app.use(cors());
+const corsOptions = {
+  origin: "https://libgen-dthraho7c-ayushs-projects-23bac148.vercel.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/auth", authRoute);
 app.use("/admin", adminroute);
 app.use("/user", userroute);

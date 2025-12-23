8bfc0d686455bba9e6c17898687d620651a51584
Ayush Singh <ayushsingh123102@gmail.com>
2025-12-24 01:40:39 +0530

    changed api location

--- Frontend/src/api/authApi.js
+++ Frontend/src/api/authApi.js
@@ -1,16 +1,12 @@
-const API_URL = "http://localhost:2000/auth";
-import axios from "axios";
+import api from "./axios";
+const API_URL = "/auth";
 export const signup = async (formData) => {
-    const res=await fetch(`${API_URL}/signup`,{
-      method:"POST",
-      headers:{"Content-Type":"application/json"},
-      body:JSON.stringify(formData),
-    });
+    const res=await api.post(`${API_URL}/signup`);
     return res.json();
 };
 export const login = async (formData) => {
   try{
-    const res=await axios.post(`${API_URL}/login`,formData);
+    const res=await api.post(`${API_URL}/login`,formData);
     return res.data;
   }catch(err){
     alert(err.message);
